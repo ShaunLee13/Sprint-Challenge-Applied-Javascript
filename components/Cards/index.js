debd45b8bf645e6cd3d94fedf9278528e131c385
Shaun Lee <shaundlee.0103@gmail.com>
2020-06-05 12:11:29 -0400

    Declared variables for cardMaker

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,11 +18,16 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+const cardContainer = document.querySelector('.cards-container')
+
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
     .then(response => {
-        console.log(`Here is your response: ${response.data.articles}`)
-        debugger
-        console.log(Object.values(response.data.articles))
+
+        Object.values(response.data.articles).forEach(subject => {
+            subject.forEach(article => {
+                console.log(article)
+            })
+        })
     })
     .catch(error => {
         console.log(`Uh oh: ${error}`)
@@ -30,3 +35,29 @@ axios.get('https://lambda-times-backend.herokuapp.com/articles')
     .finally(()=> {
         console.log('done')
     })
+
+    function cardMaker(topic){
+        //Declare Variables
+        const {headline, authorPhoto, authorName} = topic
+
+        const card = document.createElement('div')
+        const heading = document.createElement('div')
+        const author = document.createElement('div')
+        const imageCon = document.createElement('img')
+        const image = document.createElement('div')
+        const name = document.createElement('span')
+
+
+
+    }
+
+    cardMaker({headline: "AI: What Are the Ethical Ramifications of the Future?", authorPhoto: "./assets/max.jpg", authorName: "MAX GOODBOYE"})
+//  <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {author's name}</span>
+//   </div>
+// </div>
